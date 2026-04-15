ff65a5abd4fb06db1a959f19f8d351ce497ba048
flytachi <jasur.rakhmatov03@gmail.com>
2026-04-15 17:45:26 +0500

    add `LoggerAwareTrait` to `Stereotype` and initialize logger with `LoggerRegistry`

--- src/Interface/Stereotype.php
+++ src/Interface/Stereotype.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Flytachi\Winter\Base\Interface;
 
+use Flytachi\Winter\Base\Log\LoggerRegistry;
+use Psr\Log\LoggerAwareTrait;
+
 abstract class Stereotype
 {
+    use LoggerAwareTrait;
+
     public function __construct()
     {
+        self::setLogger(LoggerRegistry::instance(static::class));
     }
 }
